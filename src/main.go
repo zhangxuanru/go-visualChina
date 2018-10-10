1b15b10ce7f8cbe6f9ed99fe8e5608659a667eae
zhangxuanru <1107099011@qq.com>
2018-10-10 20:35:13 +0800

    add init

--- src/main.go
+++ src/main.go
@@ -1,56 +1,34 @@
 package main
 
 import (
-	"log"
-	"os"
+	"spider"
 	"fmt"
-	"time"
-	"io"
-	"flag"
 )
 
-const (
-	LOG_PATH_DIR = "src/log/"
+var (
+	url string
+	ok bool
 )
-
-var(
-	Info *log.Logger
-	Error *log.Logger
-	flagType int
-	flagAll  int
-	urls = []string{
-         "https://www.vcg.com/editorial",         //编辑图片
-         "https://www.vcg.com/sets/wallpaper",   //创意壁纸
-         "https://www.vcg.com/creative",        //创意图片
-         "https://www.vcg.com/design",         //设计素材
-	}
-)
-
-
-
-func init()  {
-    year :=  time.Now().Year()
-    month := time.Now().Month()
-    day := time.Now().Day()
-    infoLog := fmt.Sprintf(LOG_PATH_DIR+"info-%d-%d-%d.log",year,month,day)
-    errorLog := fmt.Sprintf(LOG_PATH_DIR+"error-%d-%d-%d.log",year,month,day)
-
-	infoFile, _ := os.OpenFile(infoLog, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
-	errorFile, _ := os.OpenFile(errorLog, os.O_CREATE|os.O_APPEND|os.O_WRONLY, os.ModePerm)
-
-	Info = log.New(infoFile,"Info: ",log.LstdFlags | log.Lshortfile | log.LUTC)
-	Error = log.New(io.MultiWriter(os.Stderr,errorFile),"error: ",log.LstdFlags | log.Lshortfile | log.LUTC)
-
-
-	//获取命令行参数
-	flag.IntVar(&flagType,"type", 0, "0:编辑图片,1:创意壁纸,2:创意图片,3:设计素材")
-	flag.IntVar(&flagAll,"all",0,"1:抓取所有,0:单个抓取")
-	flag.Parse()
-}
 
 func main(){
-
+	urls := initUlrs()
+	switch flagAll {
+	case 1:
+		spider.InitAllJobs(urls)
+	case 0:
+		if url,ok = urls[flagType];!ok{
+            Error.Fatalln("flagType 值非法，没找到相应的URL")
+		}
+		spider.InitJobs(url)
+	}
+	fmt.Println(url)
 }
+
+
+
+
+
+
 
 
 
---
+++ src/spider/job.go
@@ -0,0 +1,9 @@
+package spider
+
+func InitAllJobs(urls map[int]string)  {
+	
+}
+
+func InitJobs(url string)  {
+	
+}
